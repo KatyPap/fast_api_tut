15a943830489dd3ebfd5b450ecaf7d0effea6e82
KatyPap <katypap99>
2024-12-26 20:08:04 +0200

    first commit

---
+++ backend.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+items = ['apple','orange'] 
+
+@app.get("/")
+def root():
+  return {"Hello": "World"}
+  
+@app.get("/items")
+def get_items():
+  return items
