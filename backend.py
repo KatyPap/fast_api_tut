ab6b097931b1bbee57481f64483f31ee9021d231
Aikaterini Papanikolaou <katypap99@outlook.com>
2025-01-02 00:54:15 +0200

    up

--- backend.py
+++ backend.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI
+import psycopg2
+from pydantic import BaseModel
 
 app = FastAPI()
 
-items = ['apple','orange','banana','kiwi','strawberry'] 
+conn = psycopg2.connect(
+    host="localhost",
+    database="app_database",
+    user="katypap",
+    password="katypap",
+    port="5432"
+)
+cursor = conn.cursor()
+
+def select_from_table(table_name):
+  cursor.execute("SELECT * FROM FRUITS")
+  rows = cursor.fetchall()
+  return rows
 
 @app.get("/")
 def root():
   return {"Hello": "World"}
   
-@app.get("/items")
-def get_items():
-  return items
+@app.get("/fruits")
+def get_fruits():
+    fruits = select_from_table('FRUITS')
+    return fruits
--- dockerfile
+++ dockerfile
@@ -5,6 +5,7 @@ COPY . .
 
 RUN pip install fastapi 
 RUN pip install uvicorn
+RUN pip install psycopg2
 EXPOSE 8000
 
 CMD ["uvicorn", "backend:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
